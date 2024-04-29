fdac4de1222b2c25c618d25e92dede02d65d7472
Thiaguss <tsantos7098@gmail.com>
2024-04-29 20:08:34 -0300

    Desafio Banco Terminal DIO

---
+++ ContaBancaria/ContaTerminal/src/ContaTerminal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita e obtém os dados da conta bancária
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Limpa o buffer do scanner
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da Conta:");
+        double saldo = scanner.nextDouble();
+
+        // Exibe a mensagem com os dados inseridos
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        // Fecha o scanner
+        scanner.close();
+    }
+}
