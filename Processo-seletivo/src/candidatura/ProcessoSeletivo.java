2ee93246c04567ee6e77e2f5bea7f7d99aa75985
Thiaguss <tsantos7098@gmail.com>
2024-05-02 16:28:59 -0300

    Seleçao de candidatos

--- Processo-seletivo/src/candidatura/ProcessoSeletivo.java
+++ Processo-seletivo/src/candidatura/ProcessoSeletivo.java
@@ -1,14 +1,49 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(2000.0);
-        analisarCandidato(1500.0);
-        analisarCandidato(3000.0);
+        // analisarCandidato(2000.0);
+        // analisarCandidato(1500.0);
+        // analisarCandidato(3000.0);
+
+        selecaoCandidatos();
 
     }
 
-    static void analisarCandidato(double salarioPretendido){
+    static void selecaoCandidatos() {
+
+        String[] candidatos = { "FELIPE", "ARTUR", "MARIANA", "DEBORA", "MARCOS", "VINICIUS", "DOUGLAS", "BIANCA",
+                "VITORIA", "JESSICA" };
+        int candidatoAtual = 0;
+        int candidatosSelecionados = 0;
+        final double SALARIO_BASE = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salario " + salarioPretendido);
+
+            if (SALARIO_BASE >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            } else {
+                System.out.println("O candidato " + candidato + " não foi selecionado para a vaga");
+            }
+
+            candidatoAtual++;
+        }
+
+    }
+
+    static double valorPretendido() {
+        double valor = ThreadLocalRandom.current().nextDouble(1800.0, 2200.0);
+        return Double.parseDouble(String.format("%.2f", valor).replace(",", "."));
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
         final double SALARIO_BASE = 2000.0;
 
         if (SALARIO_BASE > salarioPretendido) {
